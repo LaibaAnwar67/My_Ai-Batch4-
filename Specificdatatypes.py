b12df7618a3911ef3311c92ed1f7cd47aa382829
LaibaAnwar67 <laibawan657@gmail.com>
2025-08-16 14:18:39 -0700

    DatatypeDeclaration

---
+++ Specificdatatypes.py
@@ -0,0 +1,50 @@
+#Specific Datatypes also through their constructor
+#Through Constructor
+strvar=str("Its me laiba.")
+print(strvar)
+print(type(strvar))
+
+# By assigning string value to the variable
+
+strvar1="Hello my name is laiba"
+print(strvar1)
+print(type(strvar1))     
+
+# Integer datatype
+
+intvar=int(45)
+print(intvar)
+print(type(intvar))
+
+# By assigning value to the datatype
+
+intvar1=46
+print(intvar1)
+print(type(intvar1))
+
+#Float DataType
+floatVar=float(40.6)
+print(floatVar)
+print(type(floatVar))
+
+#By assigning value to the datatypes
+floatVar1=80.4
+print(floatVar1)
+print(type(floatVar1))
+
+#Float with an "e" to indicate the power of 10
+FloatEVar=float(35e2)
+print(FloatEVar)
+print(type(FloatEVar))
+
+#By direct assigning the value
+FloatEvar1=80e4
+print(FloatEvar1)
+print(type(FloatEvar1))
+
+#Complex Number Representstion
+ComplexVar=complex(3+5j)
+print(type(ComplexVar))
+Complexvar2=8+9j
+print(type(Complexvar2))
+
---
+++ week1.py
@@ -0,0 +1,7 @@
+# Logical operator
+print(True and False)
+a=3
+b=6
+print((a>2) and (b>=6))
+myvar=90
+print(myvar)

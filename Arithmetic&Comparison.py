ac26164ec1a2f8748c708afc1a1f0c863dd081d9
LaibaAnwar67 <laibawan657@gmail.com>
2025-08-16 22:35:10 -0700

    Arithmetic and Comparison operator

---
+++ Arithmetic&Comparison.py
@@ -0,0 +1,27 @@
+a=4
+b=9
+#Apply different arithmetic operator
+print("Addition of two values",a+b)
+print("Subtraction of two values",a-b)
+print("Multiplication of two values",a*b)
+print("Division of two vaues",a/b)
+print("Modulus of two values",a%b)
+print("Floor_Division",a//b)
+print("power",a**b)
+
+#Comparison Operators
+#These operators return the value Either True or False
+s=9
+l=6
+#Equal to operator
+print("s==l",a==l)
+#Not equal to operator
+print("s!=l",s!=l)
+#Greater than operator
+print("s>l",s>l)
+#Less than  operator
+print("s<l",s<l)
+#Greater than equal to  operator
+print("s>=l",s>=l)
+#Less than equal to operator
+print("s<=l",s<=l)
